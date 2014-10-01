0a408d8b2b8e33171256361bac33c5adb7f8eee3
tnvl6480 <sandlafrogne@infonie.fr>
2014-10-01 16:08:00 +0200

    mots multiples

--- client/js/controllers/mainCtrl.js
+++ client/js/controllers/mainCtrl.js
@@ -1,27 +1,26 @@
 /* Contrôleur principal
  ================================================== */
 angular.module('controllers', [])
-    .controller("MainCtrl", ['$scope', '$rootScope','$http', function($scope, $rootScope,$http) {
-        $scope.init =[] ;
-        $scope.columns=[];
-        $scope.columns2=[];
-        $scope.posts=[];
-        $scope.newPhrase= [{title:''},{title:''},{title:''},{title:''},{title:''},{title:''},{title:''},{title:''},{title:''},{title:''},{title:''}]
+    .controller("MainCtrl", ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
+        $scope.init = [];
+        $scope.columns = [];
+        $scope.columns2 = [];
+        $scope.posts = [];
 
+        $http({method: 'GET', url: 'phrases.json'})
+            .success(function (data) {
+                $scope.posts = data;
+                $scope.initialisation();
+            });
 
-            $http({method: 'GET', url: 'phrases.json'})
-                .success(function(data){
-                    $scope.posts = data;
-                    $scope.initialisation();
-                });
-
-
-        $scope.initialisation = function() {
-            $scope.init =[] ;
-            $scope.columns=[];
-            $scope.columns2=[ ];
+        $scope.initialisation = function () {
+            $scope.init = [];
+            $scope.columns = [];
+            $scope.columns2 = [];
+            //choix de la phrase
             j = Math.floor((Math.random() * $scope.posts.length));
             $scope.init = $scope.posts[j]
+           //mélange des mots de la phrase
             var ordre = [];
             while (ordre.length < $scope.init.length) {
                 j = Math.floor((Math.random() * $scope.init.length));
@@ -42,57 +41,67 @@ angular.module('controllers', [])
         }
 
         $rootScope.$on('dropEvent', function (evt, dragged, dropped) {
-            var i, oldIndex1, oldIndex2,depart, destination;
+            var i, oldIndex1, oldIndex2, depart, destination;
             for (i = 0; i < $scope.columns.length; i++) {
                 var c = $scope.columns[i];
-                if (dragged.title === c.title) {
+
+                if (dragged.title === c.title && dragged.id===c.id) {
                     oldIndex1 = i;
-                    depart="c";
+                    depart = "c";
                 }
-                if (dropped.title === c.title) {
+                if (dropped.title === c.title && dropped.id=== c.id) {
                     oldIndex2 = i;
-                    destination="c"
+                    destination = "c"
                 }
             }
-
             for (j = 0; j < $scope.columns.length; j++) {
                 var d = $scope.columns2[j];
-                if (dragged.title === d.title) {
+                if (dragged.title === d.title && dragged.id===d.id) {
                     oldIndex1 = j;
-                    depart="d";
+                    depart = "d";
                 }
-                if (dropped.title === d.title) {
+                if (dropped.title === d.title && dropped.id===d.id) {
                     oldIndex2 = j;
-                    destination="d"
+                    destination = "d"
                 }
             }
 
             /*
-            C1 C2 C3 C4
-            D1 D2 D3 D4
-
+             C1 C2 C3 C4
+             D1 D2 D3 D4
              */
-
             //C4 to C2 --> C4=C2 et C2=temp
-            if(depart =='c' && destination=='c'){
+            if (depart == 'c' && destination == 'c') {
                 var temp = $scope.columns[oldIndex1];
                 $scope.columns[oldIndex1] = $scope.columns[oldIndex2];
                 $scope.columns[oldIndex2] = temp;
+
+
             }
-            //C4 to D2 --> C4=D2 et D4=temp
-            if(depart =='c' && destination=='d'){
+            //C4 to D2 --> C4=D2 et D2=temp
+            if (depart == 'c' && destination == 'd') {
                 var temp = $scope.columns[oldIndex1];
                 $scope.columns[oldIndex1] = $scope.columns2[oldIndex2];
                 $scope.columns2[oldIndex2] = temp;
+
+                if($scope.columns[oldIndex1].id == null || $scope.columns[oldIndex1].id ==undefined  ){
+                    $scope.columns[oldIndex1].id = $scope.columns[oldIndex1].title
+                    $scope.columns[oldIndex1].title='_'
+
+                }
+
             }
             //D4 to D2 --> D4=D2 et D2=temp
-            if(depart =='d' && destination=='d'){
+            if (depart == 'd' && destination == 'd') {
                 var temp = $scope.columns2[oldIndex1];
                 $scope.columns2[oldIndex1] = $scope.columns2[oldIndex2];
                 $scope.columns2[oldIndex2] = temp;
+
+
+
             }
             //D2 to C4 --> D2=C4 et C4=temp
-            if(depart =='d' && destination=='c'){
+            if (depart == 'd' && destination == 'c') {
                 var temp = $scope.columns2[oldIndex1];
                 $scope.columns2[oldIndex1] = $scope.columns[oldIndex2];
                 $scope.columns[oldIndex2] = temp;
@@ -101,15 +110,15 @@ angular.module('controllers', [])
         })
 
 
-        $scope.good=function(d,i){
-            if($scope.init[i].title == d.title)
+        $scope.good = function (d, i) {
+            if ($scope.init[i].title == d.title)
                 return true
         }
 
-        $scope.end=function(){
-            var retour=true
-            for(i=0;i<$scope.init.length;i++){
-               if($scope.init[i].title != $scope.columns2[i].title) {
+        $scope.end = function () {
+            var retour = true
+            for (i = 0; i < $scope.init.length; i++) {
+                if ($scope.init[i].title != $scope.columns2[i].title) {
                     retour = false;
                 }
             }
@@ -119,40 +128,51 @@ angular.module('controllers', [])
 
     }])
 
-    .controller("manageCtrl", ['$scope', '$rootScope','$http', function($scope, $rootScope,$http) {
+    .controller("manageCtrl", ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
         $scope.posts = [];
         $scope.newPhrase = []
-        for (i=0; i<15;i++){
-            $scope.newPhrase.push({id : i+1, title:''  }).json
+        for (i = 0; i < 15; i++) {
+            $scope.newPhrase.push({id: i + 1, title: ''  }).json
         }
-
-        console.log($scope.newPhrase)
-
         $http({method: 'GET', url: 'phrases.json'})
             .success(function (data) {
                 $scope.posts = data;
             });
 
-        $scope.addPhrase=function(){
-            var newPhraseLight=[]
-            for(i=0;i<$scope.newPhrase.length;i++) {
-                if ($scope.newPhrase[i].title != '' && $scope.newPhrase[i].title!= null  ) {
-                    console.log($scope.newPhrase[i])
+        $scope.addPhrase = function () {
+            var newPhraseLight = []
+            for (i = 0; i < $scope.newPhrase.length; i++) {
+                if ($scope.newPhrase[i].title != '' && $scope.newPhrase[i].title != null) {
                     newPhraseLight.push($scope.newPhrase[i])
                 }
             }
             $scope.posts.push(newPhraseLight)
+            save();
+
+        }
+
+        $scope.deletePhrase = function (index) {
+            $scope.posts.splice(index, 1)
+            save();
+        }
+
+        save = function () {
             $http({
                 method: 'POST',
                 url: 'json',         //this is the json file with all the information I use
                 data: $scope.posts    //this contains the modified data
-            }).success(function(response) {
-                //addLog("Success message.");
-                console.log('success')
-            }).error(function(response){
+            }).success(function (response) {
+                //réinitialisation du formulaire
+                $scope.newPhrase = []
+                for (i = 0; i < 15; i++) {
+                    $scope.newPhrase.push({id: i + 1, title: ''  }).json
+                }
+                //console.log('success')
+            }).error(function (response) {
                 //addLog("Error message.");
-                console.log('error')
+                //console.log('error')
             })
         }
+
 
     }])
--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 var express = require('express')
-    ,bodyParser = require('body-parser')
-    ,fs = require('fs')
-    ,app = express()
-    ,path =require('path')
-    ,rootPath=path.normalize(__dirname)
+    , bodyParser = require('body-parser')
+    , fs = require('fs')
+    , app = express()
+    , path = require('path')
+    , rootPath = path.normalize(__dirname)
 
 app.set('port', (process.env.PORT || 5001))
 
-app.post('/json', function(req, res){
-    console.log("Request handler 'insertUser' was called.");
-    req.on('data', function(chunk) {
-        console.log("Received body data:");
-
-        console.log(chunk.toString());
-
-console.log('rootPath : '+rootPath)
-
-        fs.writeFile(rootPath+"/client/phrases.json", chunk, function(err) {
-            if(err) {
+app.post('/json', function (req, res) {
+    req.on('data', function (chunk) {
+        fs.writeFile(rootPath + "/client/phrases.json", chunk, function (err) {
+            if (err) {
                 console.log(err);
             } else {
                 console.log("The file was saved!");
@@ -25,16 +18,14 @@ console.log('rootPath : '+rootPath)
         });
 
     });
-    req.on('end', function() {
+    req.on('end', function () {
         res.writeHead(200, "OK", {'Content-Type': 'text/html'});
         res.end();
     });
-
-
 })
 
 app.use(express.static(__dirname + '/client'))
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'))
 })
