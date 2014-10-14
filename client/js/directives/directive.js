0256b43f4a2c2402499003c9a1c02590131673af
unknown <TNVL6480@EB-GG9QV4J.ad.francetelecom.fr>
2014-10-14 11:30:00 +0200

    case bleue sous firefox + icone voiture pendant le drag

--- client/js/directives/directive.js
+++ client/js/directives/directive.js
@@ -1,15 +1,17 @@
 angular.module('directives', [])
     .directive("drag", ['$rootScope', function ($rootScope) {
 
+        var dragIcon = document.createElement('img');
+        dragIcon.src = '/icone.jpg'
         function dragStart(evt, element, dragStyle) {
-            console.log("dragStart");
+            //console.log("dragStart");
+            evt.originalEvent.dataTransfer.setDragImage(dragIcon, -10, -10);
             element.addClass(dragStyle);
             evt.originalEvent.dataTransfer.setData("id", evt.target.id);
             evt.originalEvent.dataTransfer.effectAllowed = 'move';
         };
         function dragEnd(evt, element, dragStyle) {
-            console.log("dragEnd");
-
+            //console.log("dragEnd");
             element.removeClass(dragStyle);
         };
 
@@ -33,19 +35,20 @@ angular.module('directives', [])
     .directive("drop", ['$rootScope', function ($rootScope) {
 
         function dragEnter(evt, element, dropStyle) {
-            console.log("dragEnter");
+            //console.log("dragEnter");
             evt.preventDefault();
             element.addClass(dropStyle);
         };
         function dragLeave(evt, element, dropStyle) {
-            console.log("dragLeave");
+            //console.log("dragLeave Drop");
             element.removeClass(dropStyle);
         };
-        function dragOver(evt) {
+        function dragOver(evt, element, dropStyle) {
             evt.preventDefault();
+            element.addClass(dropStyle);
         };
         function drop(evt, element, dropStyle) {
-            console.log("drop");
+            //console.log("drop");
             evt.preventDefault();
             element.removeClass(dropStyle);
         };
@@ -61,7 +64,10 @@ angular.module('directives', [])
                 element.bind('dragleave', function (evt) {
                     dragLeave(evt, element, scope.dropStyle);
                 });
-                element.bind('dragover', dragOver);
+                element.bind('dragover', function (evt) {
+                    dragOver(evt, element, scope.dropStyle);
+                });
+
                 element.bind('drop', function (evt) {
                     drop(evt, element, scope.dropStyle);
                     $rootScope.$broadcast('dropEvent', $rootScope.draggedElement, scope.dropData);
@@ -69,38 +75,3 @@ angular.module('directives', [])
             }
         }
     }])
-   /* .directive('dragEvent', ['$parse', function($parse) {
-        return function(scope, element, attrs) {
-            element.bind("dragstart", function (evt) {
-                var id = element.attr("id");
-                evt.originalEvent.dataTransfer.setData("drag-id", id);
-
-                var fn = $parse(attrs.dragEvent);
-                fn(scope, {$element : element});
-            });
-            element.attr("draggable", true);
-        }
-    }])
-    .directive('dropEvent', ['$parse', function($parse) {
-        return function(scope, element, attrs) {
-            element.bind("dragover dragenter", function (evt) {
-                evt.stopPropagation();
-                evt.preventDefault();
-
-                return false;
-            });
-
-            element.bind("drop", function (evt) {
-                var id = evt.originalEvent.dataTransfer.getData("drag-id");
-                var elementTransfer = angular.element(document.getElementById(id));
-                element.append(elementTransfer);
-
-                evt.stopPropagation();
-                evt.preventDefault();
-
-                var fn = $parse(attrs.dropEvent);
-                fn(scope, {$element : elementTransfer, $to : element});
-            });
-        }
-    }]);
-*/
